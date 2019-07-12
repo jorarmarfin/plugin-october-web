2a47666dc56eda0add0f5e57224f0ba872c82a1a
Luis mayta <luis.mayta@gmail.com>
2019-07-12 10:16:18 -0500

    Seccion config

---
+++ updates/builder_table_create_luismayta_web_config.php
@@ -0,0 +1,22 @@
+<?php namespace LuisMayta\Web\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateLuismaytaWebConfig extends Migration
+{
+    public function up()
+    {
+        Schema::create('luismayta_web_config', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('logo');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('luismayta_web_config');
+    }
+}
